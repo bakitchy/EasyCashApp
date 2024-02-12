656f63950707fe6fccba23ca13c9f13cc0495a65
bakitchy <bakitchy@gmail.com>
2024-02-12 23:08:26 +0300

    Add project files.

---
+++ EasyCashApp.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyCashApp.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
